54c90083e7eb1278caa854504db0901281da3171
Jim Mei <jim.mei@cenlting.com>
2015-10-14 16:04:33 +0800

    Just backup'd old computer to new computer. 180GB content. rsync and scp is your friends

---
+++ bash/system_backup/rsync.sh
@@ -0,0 +1,11 @@
+
+# Please note: never put / after  original dirs. Or else rsync will copy the contents in dirs
+rsync -ravzX --max-size=2g  dir1 dir2 dir3  root@${BACKUP_IP}:/backup_dir/
+
+find . -size +2G  > bigfiles.txt
+
+
+# then scp the big file. Too big file may fail with rsync (>4G) and not efficient with rsync
+
+for i in `cat bigfiles.txt`; do dirname=`dirname $i`; scp $i root@BACKUP_IP:/backup_dir/$dirname/ ; done
+
